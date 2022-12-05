2785c46f91fd082a184fef70527060178de07291
Akirami <66513481+A-kirami@users.noreply.github.com>
2022-12-05 21:17:58 +0800

    🐛 修复过高版本的语法问题

--- nonebot_plugin_chatgpt/config.py
+++ nonebot_plugin_chatgpt/config.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from nonebot import get_driver
 from pydantic import BaseModel, Extra
 
 
 class Config(BaseModel, extra=Extra.ignore):
     chatgpt_session_token: str
-    chatgpt_proxies: str | None = None
+    chatgpt_proxies: Optional[str] = None
 
 
 config = Config.parse_obj(get_driver().config)
