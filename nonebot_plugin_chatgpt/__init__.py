fc9cb48b49e91cc98818b9425f7febc19142ba2c
Akirami <66513481+A-kirami@users.noreply.github.com>
2022-12-06 02:17:34 +0800

    ✨ 允许自定义触发方式 (#4)

--- nonebot_plugin_chatgpt/__init__.py
+++ nonebot_plugin_chatgpt/__init__.py
@@ -1,8 +1,12 @@
 from collections import defaultdict
+from typing import Any, Dict, List, Type, Union
 
-from nonebot import on_message, require
+from nonebot import on_command, on_message, require
 from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.matcher import Matcher
+from nonebot.params import _command_arg
 from nonebot.rule import to_me
+from nonebot.typing import T_State
 
 from .chatgpt import Chatbot
 from .config import config
@@ -11,19 +15,40 @@ require("nonebot_plugin_apscheduler")
 
 from nonebot_plugin_apscheduler import scheduler
 
-chat = on_message(rule=to_me(), priority=999)
-
 chat_bot = Chatbot()
 
 session = defaultdict(dict)
 
 
-@chat.handle()
-async def _(event: MessageEvent) -> None:
-    text = event.get_plaintext()
+def create_matcher(
+    command: Union[str, List[str]], only_to_me: bool = True
+) -> Type[Matcher]:
+    params: Dict[str, Any] = {"priority": 999}
+
+    if command:
+        on_matcher = on_command
+        command = [command] if isinstance(command, str) else command
+        params["cmd"] = command.pop(0)
+        params["aliases"] = set(command)
+    else:
+        on_matcher = on_message
+
+    if only_to_me:
+        params["rule"] = to_me()
+
+    return on_matcher(**params)
+
+
+matcher = create_matcher(config.chatgpt_command, config.chatgpt_to_me)
+
+
+@matcher.handle()
+async def ai_chat(event: MessageEvent, state: T_State) -> None:
+    message = _command_arg(state) or event.get_message()
+    text = message.extract_plain_text().strip()
     session_id = event.get_session_id()
     msg = await chat_bot(**session[session_id]).get_chat_response(text)
-    await chat.send(msg, at_sender=True)
+    await matcher.send(msg, at_sender=True)
     session[session_id]["conversation_id"] = chat_bot.conversation_id
     session[session_id]["parent_id"] = chat_bot.parent_id
 
