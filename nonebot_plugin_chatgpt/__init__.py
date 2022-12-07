201c9525a358fce9949cc22f80c687928dac662f
Akirami <66513481+A-kirami@users.noreply.github.com>
2022-12-07 18:16:31 +0800

    ✨ 未启用图片回复的情况下不导入 `nonebot_plugin_htmlrender`

--- nonebot_plugin_chatgpt/__init__.py
+++ nonebot_plugin_chatgpt/__init__.py
@@ -15,9 +15,10 @@ require("nonebot_plugin_apscheduler")
 
 from nonebot_plugin_apscheduler import scheduler
 
-require("nonebot_plugin_htmlrender")
+if config.chatgpt_image:
+    require("nonebot_plugin_htmlrender")
 
-from nonebot_plugin_htmlrender import md_to_pic
+    from nonebot_plugin_htmlrender import md_to_pic
 
 chat_bot = Chatbot()
 
@@ -53,7 +54,6 @@ async def ai_chat(event: MessageEvent, state: T_State) -> None:
     session_id = event.get_session_id()
     msg = await chat_bot(**session[session_id]).get_chat_response(text)
     if config.chatgpt_image:
-        # 这个 AI 说话老说一半，暂时统计 ``` 数量让 MD 不至于格式错乱
         if msg.count("```") % 2 != 0:
             msg += "\n```"
         img = await md_to_pic(msg)
